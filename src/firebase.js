a344ffdb6b16f46da8ddafd82258f83cf14a665b
smskbuilds <smskbuilds@gmail.com>
2024-07-16 13:04:58 -0700

    Adding Firebase Integration

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJwOcb2C9PZzRSMhEGdU5raH8MlW1L1eA",
+  authDomain: "gamedealwizard-aa04b.firebaseapp.com",
+  projectId: "gamedealwizard-aa04b",
+  storageBucket: "gamedealwizard-aa04b.appspot.com",
+  messagingSenderId: "177132408340",
+  appId: "1:177132408340:web:1fc403e49a92bca031e101"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
