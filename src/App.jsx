8fc38505f0c08f70ff58a55c9832ead746fc8ead
smskbuilds <smskbuilds@gmail.com>
2024-07-18 10:59:35 -0700

    Buy This Game Button Updated to Pull Cheapest Deal from CheapShark
    Still struggling to get the data pull from their API to work properly. Made a local version of the API data to get the functionality working. Will work next on getting a live API pull to work!

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,56 @@ import Card from "./components/Card"
 import React from 'react'
 import rawgGameData from './gameData.js'
 import rawgGameDataSmall from './gameDataSmall.js'
+import cheapSharkDataLocal from './cheapSharkDataLocal.js'
 
 
 function App() {
 
-  const [games,setGames] = React.useState(rawgGameData.results)
-  const [surf,setSurf] = React.useState([])
+  const [gamesData,setGamesData] = React.useState(rawgGameDataSmall.results)
+  const [cheapShardData,setCheapSharkData] = React.useState([])
 
-  console.log(games)
+  // console.log([cheapShardData])
+  // console.log([cheapShardData].length)
+  // console.log(Array.isArray(cheapShardData))
+  // console.log(cheapShardData)
 
-  const cards = games.map(
-    (card) =>
+  function findCheapestDeal(dealArray) {
+    let cheapest = {}
+    for (let step = 0; step < dealArray.length; step++){
+        if(Object.keys(cheapest).length === 0){
+          cheapest = dealArray[step]
+        } else if (dealArray[step]['cheapest'] < cheapest['cheapest']) {
+          cheapest=dealArray[step]
+        }
+      }
+    return cheapest['cheapestDealID']
+    }
+
+  const cards = gamesData.map(
+    (card, index) =>
       <Card 
       key={card.id}
       heading={card.name}
       img={card.background_image}
+      link={`https://www.cheapshark.com/redirect?dealID=${findCheapestDeal(cheapSharkDataLocal[index])}`}
     />
     )
-
-  // React.useEffect(
 
   //   // I'm gonna leave this here for tonight, definitely made some progress tho! What I need to figure out is how to only call setGames once - i def need to use a mapping
   //   // function,but maybe need to map twice... Map once to get all the api info the again to setGames value
   //   // I am going to try to implement using two sets of state tomorrow!
-    
+  // need to figure out how to properly return async data within a mapping function so that it properly maps to a new object within the same array structure
+ 
+  // React.useEffect(
   //   () =>  {
-  //     const x = games.map(
-  //         (game) => 
-  //           {fetch(`https://www.cheapshark.com/api/1.0/games?title=${game.slug}`)
-  //           .then(res => res.json())
-  //           .then((data) => console.log('data'))}
-  //           )
-  //         console.log(x)
+  //     let tempCheapArray = []
+  //     for (let step = 0; step < gamesData.length; step++){
+  //       // console.log(`on step ${step}`)
+  //       {fetch(`https://www.cheapshark.com/api/1.0/games?title=${gamesData[step].slug}`)
+  //         .then(res => res.json())
+  //         .then((data) => tempCheapArray.push(data))}
+  //     }
+  //     setCheapSharkData(tempCheapArray)
   //         }
   //     ,[]
   // )
